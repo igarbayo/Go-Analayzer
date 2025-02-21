4221e394c7c42638615fe36a28c2523e92f9c31d
iggarbayo <iggarbayo@gmail.com>
2025-02-21 01:46:09 +0100

    Implementación de iniciar_analisis

--- analisis_lexico.c
+++ analisis_lexico.c
@@ -1,3 +1,14 @@
 //
 // Created by ignacio on 19/02/25.
 //
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "definiciones.h"
+#include "analisis_lexico.h"
+
+
+contenedor sig_comp_lexico() {
+    contenedor c;
+    return c;
+}
--- analisis_sintactico.c
+++ analisis_sintactico.c
@@ -2,5 +2,18 @@
 // Created by ignacio on 19/02/25.
 //
 
+#include <stdio.h>
+#include <stdlib.h>
 #include "analisis_sintactico.h"
 #include "analisis_lexico.h"
+
+void iniciar_analisis(char *nombre_fichero) {
+    contenedor c;
+    do {
+       c = sig_comp_lexico();
+       if (c.comp_lexico != FINFICHERO) {
+            printf("<%d, %s>\n", c.comp_lexico, c.lexema);
+       }
+       free(c.lexema);
+    } while (c.comp_lexico != FINFICHERO);
+}
